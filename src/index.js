885fb4fbd6fc35d8185cf9c36a5296c8782daf8b
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2021-02-01 14:34:11 -0500

    App is connected to db

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+// Requires
 const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+// Variables
 const app = express();
 const PORT = 3000;
-const bodyParser = require("body-parser");
 
 // MIDDLEWARE
 // Allows public static files to be accessed anywhere
@@ -10,10 +14,43 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Establish connection and pass credentials
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "devuser",
+    password: "",
+    database: "todo",
+});
+
+try {
+    connection.connect();
+} catch (e) {
+    console.log("Oops. connection to MySQL failed.");
+    console.log(e);
+}
+
 // Post route for add
 app.post("/add", (req, res) => {
     console.log(req.body);
-    res.send("It works!");
+    connection.query(
+        "INSERT INTO tasks (description) VALUES (?)", // Insert statement with ? as parameter
+        [req.body.item], // Values passed into parameter (?)
+        (error, result) => {
+            // check for error
+            if (error) return res.json({ error: error });
+
+            // if no error run this query
+            connection.query(
+                "SELECT LAST_INSERT_ID() FROM tasks", // Select statement to return last element inserted
+                (error, results) => {
+                    // Check for error
+                    if (error) return res.json({ error: error });
+
+                    console.log(results[0]);
+                }
+            );
+        }
+    );
 });
 
 // Listen on port
