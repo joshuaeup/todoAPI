20206487572e3a350afe0d33c5262183823643ae
Joshua Proctor <joshuaproctor@macbook-pro.lan>
2021-02-02 00:08:45 -0500

    Added API connection

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 const connection = mysql.createConnection({
     host: "localhost",
     user: "devuser",
-    password: "",
+    password: "rockeT04",
     database: "todo",
 });
 
@@ -29,9 +29,21 @@ try {
     console.log(e);
 }
 
+// Get route
+app.get("/tasks", (req, res) => {
+    connection.query(
+        "SELECT * FROM tasks ORDER BY created DESC",
+        (error, results) => {
+            if (error) return res.json({ error: error });
+
+            // Stores all non completed items in todo and completed items in completed
+            res.json(results);
+        }
+    );
+});
+
 // Post route for add
-app.post("/add", (req, res) => {
-    console.log(req.body);
+app.post("/tasks/add", (req, res) => {
     connection.query(
         "INSERT INTO tasks (description) VALUES (?)", // Insert statement with ? as parameter
         [req.body.item], // Values passed into parameter (?)
@@ -46,11 +58,39 @@ app.post("/add", (req, res) => {
                     // Check for error
                     if (error) return res.json({ error: error });
 
-                    console.log(results[0]);
+                    res.json({
+                        id: results[0]["LAST_INSERT_ID()"],
+                        description: req.body.item,
+                    });
                 }
             );
         }
     );
+});
+
+app.post("/tasks/:id/update", (req, res) => {
+    connection.query(
+        "UPDATE tasks SET completed = ? WHERE id = ?",
+        [req.body.completed, req.params.id],
+        (error, results) => {
+            if (error) return res.json({ error: error });
+
+            res.json({});
+        }
+    );
+});
+
+app.post("/tasks/:id/remove", (req, res) => {
+    connection.query(
+        "DELETE FROM tasks WHERE id = ?",
+        [req.params.id],
+        (error, results) => {
+            if (error) return res.json({ error: error });
+
+            res.json({});
+        }
+    );
+    console.log(req.params.id);
 });
 
 // Listen on port
